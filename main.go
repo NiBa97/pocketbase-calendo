ade49f87e6757c1790185f25844267af6cc1bc13
NiBa97 <niba97@icloud.com>
2025-02-24 13:35:07 +0100

    feat: Store text patches for notes

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"github.com/pocketbase/pocketbase"
 	"github.com/pocketbase/pocketbase/apis"
 	"github.com/pocketbase/pocketbase/core"
+	"github.com/sergi/go-diff/diffmatchpatch"
 )
 
 
@@ -69,6 +70,43 @@ func main() {
 
 		if err := app.Save(record); err != nil {
 			return err
+		}
+
+		return e.Next()
+	})
+
+	app.OnRecordUpdateRequest("note").BindFunc(func(e *core.RecordRequestEvent) error {
+		note := e.Record
+		oldNote, err := app.FindRecordById("note", note.GetString("id"))
+		if err != nil {
+			return err
+		}
+
+		dmp := diffmatchpatch.New()
+		oldContent := oldNote.Get("content").(string)
+		newContent := note.Get("content").(string)
+
+		// Patches are made in reverse order,
+		// so that the history can be computed from the current content
+		patches := dmp.PatchToText(dmp.PatchMake(newContent, oldContent))
+
+		newTitle := note.Get("title")
+
+		if patches != "" || newTitle != "" {
+			collection, err := app.FindCollectionByNameOrId("noteChange")
+			if err != nil {
+				return err
+			}
+
+			record := core.NewRecord(collection)
+			record.Set("note", note.Id)
+			record.Set("user", note.Get("user"))
+			record.Set("contentPatch", patches)
+			record.Set("title", newTitle)
+
+			if err := app.Save(record); err != nil {
+				return err
+			}
 		}
 
 		return e.Next()
